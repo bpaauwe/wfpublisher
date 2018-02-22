ddce3f32b827cdb61db4009b47afc33799974c19
Bob Paauwe <bpaauwe@yahoo.com>
2018-02-22 14:44:56 -0800

    Add station name to output.

--- wfp-display.c
+++ wfp-display.c
@@ -75,7 +75,7 @@ static void display_wd(struct cfg_info *cfg, struct station_info *station,
 		sprintf(trend, "steady");
 
 	/* Format the output to fit nicely on the display */
-	printf("Last Update: %s\n\n", wd->timestamp);
+	printf("%s  -- Last Update: %s\n\n", station->name, wd->timestamp);
 
 	printf("Temperature:    %5.1f%s       High:       %5.1f%s       Low:        %5.1f%s\n",
 			wd->temperature, t_str, wd->temperature_high, t_str, wd->temperature_low, t_str);
